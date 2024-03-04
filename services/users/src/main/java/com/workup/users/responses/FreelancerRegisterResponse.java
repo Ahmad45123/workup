c0e7fbd4a70f35558345467294c6124e79fbf90c
Akram-Fahim <akramadel2001@gmail.com>
2024-03-04 15:08:48 +0200

    Addressed @Ahmad45123's comments and created a test to verify the serialization process.

--- services/users/src/main/java/com/workup/users/responses/FreelancerRegisterResponse.java
+++
@@ -1,9 +0,0 @@
-package com.workup.users.responses;
-
-import com.workup.shared.commands.CommandResponse;
-import lombok.NonNull;
-
-public class FreelancerRegisterResponse extends CommandResponse {
-    @NonNull boolean success;
-    String authToken;
-}
