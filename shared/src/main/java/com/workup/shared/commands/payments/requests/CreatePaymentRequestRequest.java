fcc852476fa9f61a661f5d752b376d9085fada8e
Abdulaziz-Hassan <abdulazizhza@gmail.com>
2024-03-04 20:18:57 +0200

    Added inheritance from the Command Request and Response class to the

--- shared/src/main/java/com/workup/shared/commands/payments/requests/CreatePaymentRequestRequest.java
+++ shared/src/main/java/com/workup/shared/commands/payments/requests/CreatePaymentRequestRequest.java
@@ -1,13 +1,14 @@
 package com.workup.shared.commands.payments.requests;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.workup.shared.commands.CommandRequest;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder(setterPrefix = "with")
 @JsonDeserialize(builder = CreatePaymentRequestRequest.CreatePaymentRequestRequestBuilder.class)
-public class CreatePaymentRequestRequest {
+public class CreatePaymentRequestRequest extends CommandRequest {
     private final String clientId;
     private final String freelancerId;
     private final double amount;
