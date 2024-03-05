ed340ada9fa6ccd26441dc09386c857ea1fc94b4
marsafyyy <mokhallid72@gmail.com>
2024-03-05 22:52:18 +0200

    Updated WalletTransaction to use the naming convention

--- shared/src/main/java/com/workup/shared/commands/payments/WalletTransaction.java
+++ shared/src/main/java/com/workup/shared/commands/payments/WalletTransaction.java
@@ -4,10 +4,10 @@ import java.util.Date;
 
 public class WalletTransaction {
     private String id;
-    private String wallet_id;
+    private String walletId;
     private double amount;
-    private String payment_transaction_id;
+    private String paymentTransactionId;
     private String description;
-    private Date created_at;
-    private String transaction_type;
+    private Date createdAt;
+    private String transactionType;
 }
