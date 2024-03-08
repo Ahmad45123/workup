7621e89c8ab57644fc7b13fde256d0f9b02f3bee
Akram-Fahim <akramadel2001@gmail.com>
2024-03-08 22:41:14 +0200

    Revert "Deleted commented file"
    This reverts commit f9b11cf2487813bd69bb8f2fe0ffab8a848ea00a.

---
+++ shared/src/main/java/com/workup/shared/commands/users/requests/FreelancerLoginRequest.java
@@ -0,0 +1,9 @@
+//package com.workup.users.requests;
+//
+//public class FreelancerLoginRequest {
+//    String email = "";
+//
+//    @NonNull
+//    @Builder.Default
+//    String passwordHash = "";
+//}
