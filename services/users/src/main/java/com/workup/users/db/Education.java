80aa8471f161611f9ec88a37fc33c3d517588182
Akram Fahim <akramadel2001@gmail.com>
2024-03-12 01:31:41 +0200

    Created Users DB Schema (#5)
    Added the DB Schema discussed in the meeting with all of its required dependencies
    
    URL: https://docs.spring.io/spring-data/mongodb/reference/mongodb/mapping/document-references.html

---
+++ services/users/src/main/java/com/workup/users/db/Education.java
@@ -0,0 +1,28 @@
+package com.workup.users.db;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Builder(setterPrefix = "with")
+@Getter
+@Setter
+@Document
+public class Education {
+    @Id
+    private ObjectId id;
+
+    private String school_name;
+    private String degree;
+    private Date education_start_date;
+    private String city;
+    private Date end_date;
+    private String major;
+    private String education_description;
+    private String grade;
+}
