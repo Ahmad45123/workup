5bf05140e15c1c7090a5ae6ccb8e55c74387b6c2
Ziad A.Sadek <ziadsadek999@gmail.com>
2024-03-19 14:32:03 +0200

    Create proposal response

--- shared/src/main/java/com/workup/shared/commands/jobs/proposals/CreateProposalResponse.java
+++ shared/src/main/java/com/workup/shared/commands/jobs/proposals/CreateProposalResponse.java
@@ -1,4 +1,15 @@
 package com.workup.shared.commands.jobs.proposals;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
 public class CreateProposalResponse {
+    public String id;
+
+    @JsonCreator
+    public CreateProposalResponse(@JsonProperty("id") String id) {
+        this.id = id;
+    }
 }
