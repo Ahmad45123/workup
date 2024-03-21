a5d92d784929cb29bb4087b90f836e6d37062901
Elshimaa Betah <79271594+ShimaaBetah@users.noreply.github.com>
2024-03-22 00:45:12 +0200

    Add shared CommandMap class (#14)

---
+++ services/jobs/src/main/java/com/workup/jobs/commands/JobCommand.java
@@ -0,0 +1,9 @@
+package com.workup.jobs.commands;
+
+import com.workup.shared.commands.Command;
+import com.workup.shared.commands.CommandRequest;
+
+public abstract class JobCommand implements Command{
+   // here will add any instance variables you need for your command logic . EX: Repository
+    
+}
