c016f19d6a277f501caad143e2b9fdef387c513e
Abdelrahman Elsalh <abdof1723@gmail.com>
2024-03-22 22:17:06 +0200

    Get Job By Id request/response

---
+++ shared/src/main/java/com/workup/shared/commands/jobs/responses/GetJobByIdResponse.java
@@ -0,0 +1,26 @@
+package com.workup.shared.commands.jobs.responses;
+
+
+import java.sql.Date;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.workup.shared.Enums.Jobs.Experience;
+
+import lombok.Builder;
+import lombok.Getter;
+@Getter
+@Builder(setterPrefix = "with")
+@JsonDeserialize(builder = GetJobByIdResponse.GetJobByIdResponseBuilder.class)
+public class GetJobByIdResponse {
+    private final String id;
+    private final String title;
+    private final String description;
+    private final String location;
+    private final double budget;
+    private final String[] skills;
+    private final Experience experience;
+    private final String clientId;
+    private final boolean isActive;
+    private final Date createdAt;
+    private final Date modifiedAt;
+}
