ff34b77d1b83f672caba9b9564116af1371c9d28
Karim Elmosalamy <karimosalamy@gmail.com>
2024-03-24 23:14:08 +0200

    added contracts initial shared apis

---
+++ services/contract/src/test/java/com/contract/ContractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
