bcb2789bf6223a427a3f6e918f6a6640df2d9703
Karim Elmosalamy <karimosalamy@gmail.com>
2024-03-24 23:14:08 +0200

    added contracts initial shared apis

---
+++ services/contract/src/main/java/com/contract/ContractApplication.java
@@ -0,0 +1,13 @@
+package com.contract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractApplication.class, args);
+    }
+
+}
