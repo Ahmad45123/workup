43379cc8a560acdf3e2e2ca8d14f83fb91494a4a
Karim Elmosalamy <karimosalamy@gmail.com>
2024-04-01 12:31:11 +0200

    added requested changes

--- shared/src/main/java/com/workup/shared/commands/contracts/Milestone.java
+++ shared/src/main/java/com/workup/shared/commands/contracts/Milestone.java
@@ -5,7 +5,7 @@ import com.workup.shared.enums.contracts.MilestoneState;
 import lombok.Builder;
 
 @Builder(setterPrefix = "with")
-@JsonDeserialize
+@JsonDeserialize(builder = com.workup.shared.commands.contracts.Milestone.class)
 public class Milestone {
     private final String milestoneId;
     private final String contractId;
