d8335d2cfa498fec653a186825f0a34f4c37374f
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2024-04-18 23:14:42 +0200

    feat: complete project setup (#40)
    * feat: complete project setup
    
    Add docker-compose.yml.
    Rename Contract to Contracts service.
    Get DB and App running.
    
    * gitignore.
    
    * Conflict.

--- services/contracts/src/main/java/com/workup/contracts/ContractsApplication.java
+++ services/contracts/src/main/java/com/workup/contracts/ContractsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContractApplication {
+public class ContractsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ContractApplication.class, args);
+        SpringApplication.run(ContractsApplication.class, args);
     }
 
 }
