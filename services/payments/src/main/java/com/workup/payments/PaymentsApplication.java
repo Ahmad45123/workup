f0360547824b28604fdeb44f7ea9d8ccaa15e4f6
Abdulaziz-Hassan <abdulazizhza@gmail.com>
2024-04-19 23:46:54 +0200

    Add RabbitMQ setup

--- services/payments/src/main/java/com/workup/payments/PaymentsApplication.java
+++ services/payments/src/main/java/com/workup/payments/PaymentsApplication.java
@@ -1,7 +1,13 @@
 package com.workup.payments;
 
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PaymentsApplication {
@@ -10,4 +16,20 @@ public class PaymentsApplication {
         SpringApplication.run(PaymentsApplication.class, args);
     }
 
+    @Bean
+    public ApplicationRunner runner(AmqpTemplate template) {
+        return args -> {
+            // TODO: Test each command
+        };
+    }
+
+    @Bean
+    public Queue myQueue() {
+        return new Queue("paymentsqueue");
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 }
---
+++ services/payments/src/main/java/com/workup/payments/RabbitMQListener.java
@@ -0,0 +1,18 @@
+package com.workup.payments;
+
+import com.workup.payments.commands.PaymentCommandMap;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RabbitListener(queues = "paymentsqueue")
+public class RabbitMQListener {
+
+    @Autowired
+    public PaymentCommandMap commandMap;
+
+    // TODO: Implement receive method for each command
+
+}
