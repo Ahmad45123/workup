16654064cfeac9ba296d7c5a4b1ebd5a77d15b36
HusseinYasser <husseinyasser388@gmail.com>
2024-04-20 10:09:41 +0200

    added validation on the request contract termination command

--- services/contracts/src/main/java/com/workup/contracts/commands/RequestContractTerminationCommand.java
+++ services/contracts/src/main/java/com/workup/contracts/commands/RequestContractTerminationCommand.java
@@ -1,5 +1,6 @@
 package com.workup.contracts.commands;
 
+import com.workup.contracts.models.Contract;
 import com.workup.contracts.models.TerminationRequest;
 import com.workup.shared.commands.contracts.requests.ContractTerminationRequest;
 import com.workup.shared.commands.contracts.responses.ContractTerminationResponse;
@@ -7,13 +8,33 @@ import com.workup.shared.enums.contracts.TerminationRequestStatus;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class RequestContractTerminationCommand extends ContractCommand<ContractTerminationRequest, ContractTerminationResponse>{
 
+    private boolean isValidRequest(ContractTerminationRequest request)
+    {
+        //check if the contract id is a valid contract
+        Optional<Contract> contract = contractRepository.findById(UUID.fromString(request.getContractId()));
+        if(contract.isEmpty())
+        {
+            return false;
+        }
+        //check that requester id is a part of the contract
+        return contract.get().getClientId().equals(request.getUserId())
+                || contract.get().getFreelancerId().equals(request.getUserId());
+
+    }
+
     @Override
     public ContractTerminationResponse Run(ContractTerminationRequest request)
     {
+        if(!isValidRequest(request))
+        {
+            return ContractTerminationResponse.builder().withSuccess(false).build();
+        }
+
         //Check if there is a termination request pending for this user on that contract
         List<TerminationRequest> existedRequests = terminationRequestRepository.findByRequesterIdAndContractIdAndStatus(request.getUserId(), request.getContractId(), TerminationRequestStatus.PENDING);
         if(!existedRequests.isEmpty())
