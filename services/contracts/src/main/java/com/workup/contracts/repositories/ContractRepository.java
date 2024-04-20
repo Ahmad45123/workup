c678354702384594d6130ba265a004fb10581b20
HusseinYasser <husseinyasser388@gmail.com>
2024-04-20 02:04:23 +0200

    added @repo annotation
    added @repository annotation so that spring makes sure these beans are created before autowiring them elsewhere

--- services/contracts/src/main/java/com/workup/contracts/repositories/ContractRepository.java
+++ services/contracts/src/main/java/com/workup/contracts/repositories/ContractRepository.java
@@ -5,7 +5,9 @@ import java.util.UUID;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
 import com.workup.contracts.models.Contract;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContractRepository extends CassandraRepository<Contract, UUID>{
 
 }
