a9f281c1293b1ad6a66f552a8679cdb1824d8539
HusseinYasser <96487516+HusseinYasser@users.noreply.github.com>
2024-04-20 07:47:44 +0200

    Feat/contract and payment commands (#42)
    * Updated the ContractTerminationRequest
    
    added reason string in the request, and changed the status from string
    to enum.
    
    * added the req/res template for handling termination requests
    
    * added the termination request model
    
    * added terminationReq repository and added contractId to the terminationRequest model
    
    * implemented the contract termination req command
    
    * removed unscessary toString()
    
    * added @repo annotation
    
    added @repository annotation so that spring makes sure these beans are created before autowiring them elsewhere
    
    * added the requestContractTermination command to the map
    
    And changed the setters to lombok in the contractCommand and added
    missing autowired annotations in the map

---
+++ services/contracts/src/main/java/com/workup/contracts/models/TerminationRequest.java
@@ -0,0 +1,23 @@
+package com.workup.contracts.models;
+
+import com.workup.shared.enums.contracts.TerminationRequestStatus;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.util.UUID;
+
+@Getter
+@Builder(setterPrefix = "with")
+@Table("termination_requests")
+public class TerminationRequest {
+
+    @PrimaryKey
+    private UUID requestId;
+    private String contractId;
+    private String requesterId;
+    private String reason;
+    private TerminationRequestStatus status;
+
+}
--- services/contracts/src/main/java/com/workup/contracts/repositories/ContractMilestoneRepository.java
+++ services/contracts/src/main/java/com/workup/contracts/repositories/ContractMilestoneRepository.java
@@ -4,7 +4,9 @@ import java.util.UUID;
 
 import com.workup.contracts.models.ContractMilestone;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContractMilestoneRepository extends CassandraRepository<ContractMilestone, UUID>{
 
 }
