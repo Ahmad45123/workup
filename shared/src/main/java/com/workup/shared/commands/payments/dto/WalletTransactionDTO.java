1adeec399bce651d2cd6149c99b2be9c24968a5f
Abdulaziz Hassan <abdulazizhza@gmail.com>
2024-04-21 21:56:06 +0200

    Update shared/src/main/java/com/workup/shared/commands/payments/dto/WalletTransactionDTO.java
    Co-authored-by: Ahmed Elwasefi <a.m.elwasefi@gmail.com>

--- shared/src/main/java/com/workup/shared/commands/payments/dto/WalletTransactionDTO.java
+++ shared/src/main/java/com/workup/shared/commands/payments/dto/WalletTransactionDTO.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder(setterPrefix = "with")
-@JsonDeserialize
+@Jacksonized
 public class WalletTransactionDTO {
 
   private String id;
