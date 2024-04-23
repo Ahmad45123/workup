fac350ae4d44950d80b60d3094bc74aa573b7d15
Abdelrahman Elsalh <abdof1723@gmail.com>
2024-04-23 02:38:00 +0200

    handle exception

--- services/jobs/src/main/java/com/workup/jobs/commands/GetProposalsByJobIdCommand.java
+++ services/jobs/src/main/java/com/workup/jobs/commands/GetProposalsByJobIdCommand.java
@@ -8,6 +8,7 @@ import com.workup.shared.commands.jobs.proposals.ProposalMilestone;
 import com.workup.shared.commands.jobs.proposals.ProposalModel;
 import com.workup.shared.commands.jobs.proposals.requests.GetProposalsByJobIdRequest;
 import com.workup.shared.commands.jobs.proposals.responses.GetProposalsByJobIdResponse;
+import com.workup.shared.enums.HttpStatusCode;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,47 +17,55 @@ public class GetProposalsByJobIdCommand
 
   @Override
   public GetProposalsByJobIdResponse Run(GetProposalsByJobIdRequest request) {
-    List<Proposal> response = proposalRepository.findByJobId(request.getJobId());
+    try {
+      List<Proposal> response = proposalRepository.findByJobId(request.getJobId());
 
-    List<ProposalModel> proposals = new ArrayList<>();
-    for (Proposal proposal : response) {
-      ArrayList<ProposalAttachment> attachments = new ArrayList<>();
-      for (Attachment attachment : proposal.getAttachments()) {
-        attachments.add(
-          ProposalAttachment
+      List<ProposalModel> proposals = new ArrayList<>();
+      for (Proposal proposal : response) {
+        ArrayList<ProposalAttachment> attachments = new ArrayList<>();
+        for (Attachment attachment : proposal.getAttachments()) {
+          attachments.add(
+            ProposalAttachment
+              .builder()
+              .withName(attachment.getName())
+              .withName(attachment.getUrl())
+              .build()
+          );
+        }
+        ArrayList<ProposalMilestone> milestones = new ArrayList<>();
+        for (Milestone milestone : proposal.getMilestones()) {
+          milestones.add(
+            ProposalMilestone
+              .builder()
+              .withAmount(milestone.getAmount())
+              .withDescription(milestone.getDescription())
+              .withDueDate(milestone.getDueDate())
+              .build()
+          );
+        }
+        proposals.add(
+          ProposalModel
             .builder()
-            .withName(attachment.getName())
-            .withName(attachment.getUrl())
+            .withId(proposal.getPrimaryKey().getId().toString())
+            .withJobId(proposal.getPrimaryKey().getJobId())
+            .withFreelancerId(proposal.getFreelancerId())
+            .withCoverLetter(proposal.getCoverLetter())
+            .withStatus(proposal.getStatus())
+            .withCreatedAt(proposal.getCreatedAt())
+            .withModifiedAt(proposal.getUpdatedAt())
+            .withDuration(proposal.getDuration())
+            .withAttachments(attachments)
+            .withMilestones(milestones)
             .build()
         );
       }
-      ArrayList<ProposalMilestone> milestones = new ArrayList<>();
-      for (Milestone milestone : proposal.getMilestones()) {
-        milestones.add(
-          ProposalMilestone
-            .builder()
-            .withAmount(milestone.getAmount())
-            .withDescription(milestone.getDescription())
-            .withDueDate(milestone.getDueDate())
-            .build()
-        );
-      }
-      proposals.add(
-        ProposalModel
-          .builder()
-          .withId(proposal.getPrimaryKey().getId().toString())
-          .withJobId(proposal.getPrimaryKey().getJobId())
-          .withFreelancerId(proposal.getFreelancerId())
-          .withCoverLetter(proposal.getCoverLetter())
-          .withStatus(proposal.getStatus())
-          .withCreatedAt(proposal.getCreatedAt())
-          .withModifiedAt(proposal.getUpdatedAt())
-          .withDuration(proposal.getDuration())
-          .withAttachments(attachments)
-          .withMilestones(milestones)
-          .build()
-      );
+      return GetProposalsByJobIdResponse.builder().withProposals(proposals).build();
+    } catch (Exception e) {
+      return GetProposalsByJobIdResponse
+        .builder()
+        .withStatusCode(HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .withErrorMessage("An error occurred while fetching proposals")
+        .build();
     }
-    return GetProposalsByJobIdResponse.builder().withProposals(proposals).build();
   }
 }
