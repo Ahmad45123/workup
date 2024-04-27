bedd812a60acd3f61027e6b480ee70b6501c1b4a
Mahmoud Gaber <mahmoudhosam44@gmail.com>
2024-04-27 16:53:20 +0300

    feat: Add Freelancer language CRUD

---
+++ services/users/src/main/java/com/workup/users/commands/AddFreelancerLanguageCommand.java
@@ -0,0 +1,21 @@
+package com.workup.users.commands;
+
+import com.workup.shared.commands.users.requests.AddFreelancerLanguageRequest;
+import com.workup.shared.commands.users.responses.AddFreelancerLanguageResponse;
+import com.workup.users.db.Freelancer;
+
+import java.util.Optional;
+
+public class AddFreelancerLanguageCommand extends UserCommand<AddFreelancerLanguageRequest, AddFreelancerLanguageResponse>{
+
+    @Override
+    public AddFreelancerLanguageResponse Run(AddFreelancerLanguageRequest request) {
+        Optional<Freelancer> freelancerOptional = freelancerRepository.findById(request.getUser_id());
+        if (freelancerOptional.isEmpty())
+            return AddFreelancerLanguageResponse.builder().withSuccess(false).build();
+        Freelancer freelancer = freelancerOptional.get();
+        freelancer.getLanguages().add(request.getNewLanguage());
+        freelancerRepository.save(freelancer);
+        return AddFreelancerLanguageResponse.builder().withSuccess(true).build();
+    }
+}
