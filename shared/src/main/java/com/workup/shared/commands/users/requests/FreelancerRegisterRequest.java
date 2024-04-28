a2d9f3c2ef2cc6a59e39110746f9cf0fefe42695
Akram-Fahim <akramadel2001@gmail.com>
2024-04-28 21:29:40 +0300

    Updated the annotations to match the ones requested by @Ahmad45123
    Signed-off-by: Akram-Fahim <akramadel2001@gmail.com>

--- shared/src/main/java/com/workup/shared/commands/users/requests/FreelancerRegisterRequest.java
+++ shared/src/main/java/com/workup/shared/commands/users/requests/FreelancerRegisterRequest.java
@@ -1,20 +1,19 @@
 package com.workup.shared.commands.users.requests;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.workup.shared.commands.CommandRequest;
-import lombok.Builder;
-import lombok.Getter;
-
 import java.util.Date;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
 
 @Getter
-@Builder(setterPrefix = "with")
-@JsonDeserialize(builder = FreelancerRegisterRequest.FreelancerRegisterRequestBuilder.class)
+@SuperBuilder(setterPrefix = "with")
+@Jacksonized
 public class FreelancerRegisterRequest extends CommandRequest {
-    private String email;
-    private String passwordHash;
-    private String fullName;
-    private String jobTitle;
-    private String city;
-    private Date birthDate;
+  private String email;
+  private String passwordHash;
+  private String fullName;
+  private String jobTitle;
+  private String city;
+  private Date birthDate;
 }
--- shared/src/main/java/com/workup/shared/commands/users/responses/AuthenticationResponse.java
+++ shared/src/main/java/com/workup/shared/commands/users/responses/AuthenticationResponse.java
@@ -1,13 +1,13 @@
 package com.workup.shared.commands.users.responses;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
 
 @Getter
-@Builder(setterPrefix = "with")
-@JsonDeserialize(builder = AuthenticationResponse.AuthenticationResponseBuilder.class)
+@SuperBuilder(setterPrefix = "with")
+@Jacksonized
 public class AuthenticationResponse {
-    private String email;
-    private UserType userType;
+  private String email;
+  private UserType userType;
 }
