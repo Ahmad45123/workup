3c6d6b6c668c1f7487562d49e430381984e3f51f
Akram-Fahim <akramadel2001@gmail.com>
2024-04-28 22:07:27 +0300

    Linting
    Signed-off-by: Akram-Fahim <akramadel2001@gmail.com>

--- services/users/src/test/java/com/workup/users/RequestsAndResponsesTest.java
+++ services/users/src/test/java/com/workup/users/RequestsAndResponsesTest.java
@@ -1,5 +1,7 @@
 package com.workup.users;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.workup.shared.commands.users.requests.AuthenticationRequest;
@@ -9,31 +11,27 @@ import com.workup.shared.commands.users.requests.LoginRequest;
 import com.workup.shared.commands.users.responses.SignUpAndInResponse;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
 public class RequestsAndResponsesTest {
-    private static Object[] testObjects = { // all requests/responses to be tested
-            FreelancerRegisterRequest.builder().build(),
-            SignUpAndInResponse.builder().build(),
-            AuthenticationRequest.builder().build(),
-            ClientRegisterRequest.builder().build(),
-            LoginRequest.builder().build()
-    };
+  private static Object[] testObjects = { // all requests/responses to be tested
+    FreelancerRegisterRequest.builder().build(),
+    SignUpAndInResponse.builder().build(),
+    AuthenticationRequest.builder().build(),
+    ClientRegisterRequest.builder().build(),
+    LoginRequest.builder().build()
+  };
 
-    @Test
-    public void testSerializationNoExceptionsThrown() throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        for (Object testObject : testObjects) {
-            String className = testObject.getClass().getName();
-            assertDoesNotThrow(
-                    () -> objectMapper.writeValueAsString(testObject),
-                    String.format("Serialization of %s should not throw exceptions", className)
-            );
-            String json = objectMapper.writeValueAsString(testObject);
-            assertDoesNotThrow(
-                    () -> objectMapper.readValue(json, testObject.getClass()),
-                    String.format("Deserialization of %s should not throw exceptions", className)
-            );
-        }
+  @Test
+  public void testSerializationNoExceptionsThrown() throws JsonProcessingException {
+    ObjectMapper objectMapper = new ObjectMapper();
+    for (Object testObject : testObjects) {
+      String className = testObject.getClass().getName();
+      assertDoesNotThrow(
+          () -> objectMapper.writeValueAsString(testObject),
+          String.format("Serialization of %s should not throw exceptions", className));
+      String json = objectMapper.writeValueAsString(testObject);
+      assertDoesNotThrow(
+          () -> objectMapper.readValue(json, testObject.getClass()),
+          String.format("Deserialization of %s should not throw exceptions", className));
     }
+  }
 }
