726d07d37c9738d3498e6a4b047ec80eacb39c17
Abdulaziz-Hassan <abdulazizhza@gmail.com>
2024-04-29 16:43:08 +0300

    Delete old testing setup

--- services/payments/src/main/java/com/workup/payments/PaymentsApplication.java
+++ services/payments/src/main/java/com/workup/payments/PaymentsApplication.java
@@ -1,12 +1,9 @@
 package com.workup.payments;
 
-import com.workup.shared.commands.payments.paymentrequest.requests.CreatePaymentRequestRequest;
 import com.workup.shared.enums.ServiceQueueNames;
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,20 +13,6 @@ public class PaymentsApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(PaymentsApplication.class, args);
-  }
-
-  @Bean
-  public ApplicationRunner runner(AmqpTemplate template) {
-    return args -> {
-      CreatePaymentRequestRequest createPaymentRequest =
-          CreatePaymentRequestRequest.builder()
-              .withAmount(1200)
-              .withDescription("Payment for services rendered")
-              .withClientId("3")
-              .withFreelancerId("4")
-              .build();
-      template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, createPaymentRequest);
-    };
   }
 
   @Bean
