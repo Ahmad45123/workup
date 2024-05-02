736cd5eb716c20c9b0aea265c07ae1aeb3b45ec5
Abdulaziz Hassan <abdulazizhza@gmail.com>
2024-05-02 16:01:36 +0300

    Add `PaymentRequest` commands tests (#57)
    * Add `PaymentRequest` commands tests
    
    * Add negative test case scenarios for `GetPaymentRequestCommand` and `PayPaymentRequestCommand`

--- services/payments/src/main/java/com/workup/payments/RabbitMQListener.java
+++ services/payments/src/main/java/com/workup/payments/RabbitMQListener.java
@@ -1,11 +1,11 @@
 package com.workup.payments;
 
 import com.workup.payments.commands.PaymentCommandMap;
-import com.workup.payments.commands.paymentrequest.CreatePaymentRequestCommand;
+import com.workup.payments.commands.paymentrequest.*;
 import com.workup.payments.commands.wallet.CreateWalletCommand;
 import com.workup.payments.commands.wallet.GetWalletCommand;
-import com.workup.shared.commands.payments.paymentrequest.requests.CreatePaymentRequestRequest;
-import com.workup.shared.commands.payments.paymentrequest.responses.CreatePaymentRequestResponse;
+import com.workup.shared.commands.payments.paymentrequest.requests.*;
+import com.workup.shared.commands.payments.paymentrequest.responses.*;
 import com.workup.shared.commands.payments.wallet.requests.CreateWalletRequest;
 import com.workup.shared.commands.payments.wallet.requests.GetWalletRequest;
 import com.workup.shared.commands.payments.wallet.responses.CreateWalletResponse;
@@ -20,12 +20,36 @@ import org.springframework.stereotype.Service;
 @RabbitListener(queues = ServiceQueueNames.PAYMENTS)
 public class RabbitMQListener {
 
-  @Autowired
-  public PaymentCommandMap commandMap;
+  @Autowired public PaymentCommandMap commandMap;
 
   @RabbitHandler
   public CreatePaymentRequestResponse receive(CreatePaymentRequestRequest in) throws Exception {
     return ((CreatePaymentRequestCommand) commandMap.getCommand("CreatePaymentRequest")).Run(in);
+  }
+
+  @RabbitHandler
+  public GetClientPaymentRequestsResponse receive(GetClientPaymentRequestsRequest in)
+      throws Exception {
+    return ((GetClientPaymentRequestsCommand) commandMap.getCommand("GetClientPaymentRequests"))
+        .Run(in);
+  }
+
+  @RabbitHandler
+  public GetFreelancerPaymentRequestsResponse receive(GetFreelancerPaymentRequestsRequest in)
+      throws Exception {
+    return ((GetFreelancerPaymentRequestsCommand)
+            commandMap.getCommand("GetFreelancerPaymentRequests"))
+        .Run(in);
+  }
+
+  @RabbitHandler
+  public GetPaymentRequestResponse receive(GetPaymentRequestRequest in) throws Exception {
+    return ((GetPaymentRequestCommand) commandMap.getCommand("GetPaymentRequest")).Run(in);
+  }
+
+  @RabbitHandler
+  public PayPaymentRequestResponse receive(PayPaymentRequestRequest in) throws Exception {
+    return ((PayPaymentRequestCommand) commandMap.getCommand("PayPaymentRequest")).Run(in);
   }
 
   @RabbitHandler
--- services/payments/src/test/java/com/workup/payments/PaymentsApplicationTests.java
+++ services/payments/src/test/java/com/workup/payments/PaymentsApplicationTests.java
@@ -2,14 +2,27 @@ package com.workup.payments;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.workup.payments.models.PaymentRequest;
+import com.workup.payments.models.PaymentTransaction;
+import com.workup.payments.models.Wallet;
+import com.workup.payments.models.WalletTransaction;
 import com.workup.payments.repositories.PaymentRequestRepository;
 import com.workup.payments.repositories.PaymentTransactionRepository;
 import com.workup.payments.repositories.WalletRepository;
 import com.workup.payments.repositories.WalletTransactionRepository;
-import com.workup.shared.commands.payments.paymentrequest.requests.CreatePaymentRequestRequest;
-import com.workup.shared.commands.payments.paymentrequest.responses.CreatePaymentRequestResponse;
+import com.workup.shared.commands.payments.dto.PaymentRequestDTO;
+import com.workup.shared.commands.payments.paymentrequest.requests.*;
+import com.workup.shared.commands.payments.paymentrequest.responses.*;
+import com.workup.shared.commands.payments.wallet.requests.CreateWalletRequest;
+import com.workup.shared.commands.payments.wallet.requests.GetWalletRequest;
+import com.workup.shared.commands.payments.wallet.responses.CreateWalletResponse;
+import com.workup.shared.commands.payments.wallet.responses.GetWalletResponse;
 import com.workup.shared.enums.HttpStatusCode;
 import com.workup.shared.enums.ServiceQueueNames;
+import com.workup.shared.enums.payments.PaymentRequestStatus;
+import com.workup.shared.enums.payments.PaymentTransactionStatus;
+import com.workup.shared.enums.payments.WalletTransactionType;
+import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,10 +37,6 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import com.workup.shared.commands.payments.wallet.requests.CreateWalletRequest;
-import com.workup.shared.commands.payments.wallet.responses.CreateWalletResponse;
-import com.workup.shared.commands.payments.wallet.requests.GetWalletRequest;
-import com.workup.shared.commands.payments.wallet.responses.GetWalletResponse;
 
 @SpringBootTest
 @Testcontainers
@@ -35,21 +44,18 @@ import com.workup.shared.commands.payments.wallet.responses.GetWalletResponse;
 class PaymentsApplicationTests {
 
   @Container
-  static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest");
+  static final PostgreSQLContainer<?> postgreSQLContainer =
+      new PostgreSQLContainer<>("postgres:latest");
 
   @Container
-  static final RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq:3.13-management");
+  static final RabbitMQContainer rabbitMQContainer =
+      new RabbitMQContainer("rabbitmq:3.13-management");
 
-  @Autowired
-  private AmqpTemplate template;
-  @Autowired
-  private PaymentRequestRepository paymentRequestRepository;
-  @Autowired
-  private PaymentTransactionRepository paymentTransactionRepository;
-  @Autowired
-  private WalletRepository walletRepository;
-  @Autowired
-  private WalletTransactionRepository walletTransactionRepository;
+  @Autowired private AmqpTemplate template;
+  @Autowired private PaymentRequestRepository paymentRequestRepository;
+  @Autowired private PaymentTransactionRepository paymentTransactionRepository;
+  @Autowired private WalletRepository walletRepository;
+  @Autowired private WalletTransactionRepository walletTransactionRepository;
 
   @BeforeEach
   void clearAll() {
@@ -80,14 +86,16 @@ class PaymentsApplicationTests {
 
   @Test
   void testCreatePaymentRequest() {
-    CreatePaymentRequestRequest createPaymentRequest = CreatePaymentRequestRequest.builder()
-        .withAmount(1200)
-        .withDescription("Payment for services rendered")
-        .withClientId("3")
-        .withFreelancerId("4")
-        .build();
-    CreatePaymentRequestResponse response = (CreatePaymentRequestResponse) template
-        .convertSendAndReceive(ServiceQueueNames.PAYMENTS, createPaymentRequest);
+    CreatePaymentRequestRequest createPaymentRequest =
+        CreatePaymentRequestRequest.builder()
+            .withAmount(1200)
+            .withDescription("Payment for services rendered")
+            .withClientId("3")
+            .withFreelancerId("4")
+            .build();
+    CreatePaymentRequestResponse response =
+        (CreatePaymentRequestResponse)
+            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, createPaymentRequest);
 
     assertNotNull(response);
     assertEquals(HttpStatusCode.CREATED, response.getStatusCode());
@@ -105,49 +113,311 @@ class PaymentsApplicationTests {
   }
 
   @Test
+  void testGetClientPaymentRequests() {
+    PaymentRequest paymentRequest1 =
+        PaymentRequest.builder()
+            .withAmount(1200)
+            .withDescription("Payment for services rendered")
+            .withClientId("3")
+            .withFreelancerId("4")
+            .build();
+    paymentRequestRepository.save(paymentRequest1);
+
+    PaymentRequest paymentRequest2 =
+        PaymentRequest.builder()
+            .withAmount(3600)
+            .withDescription("Payment for translation services")
+            .withClientId("3")
+            .withFreelancerId("10")
+            .build();
+    paymentRequestRepository.save(paymentRequest2);
+
+    GetClientPaymentRequestsRequest getClientPaymentRequests =
+        GetClientPaymentRequestsRequest.builder().withClientId("3").build();
+
+    GetClientPaymentRequestsResponse response =
+        (GetClientPaymentRequestsResponse)
+            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, getClientPaymentRequests);
+
+    assertNotNull(response);
+    assertEquals(HttpStatusCode.OK, response.getStatusCode());
+
+    assertNotNull(response.getRequests());
+    assertEquals(2, response.getRequests().size());
+
+    PaymentRequestDTO requestDTO1 = response.getRequests().get(0);
+    PaymentRequestDTO requestDTO2 = response.getRequests().get(1);
+
+    assertAll(
+        () -> assertEquals(paymentRequest1.getId(), requestDTO1.getId()),
+        () -> assertEquals(paymentRequest1.getAmount(), requestDTO1.getAmount()),
+        () -> assertEquals(paymentRequest1.getDescription(), requestDTO1.getDescription()),
+        () -> assertEquals(paymentRequest1.getClientId(), requestDTO1.getClientId()),
+        () -> assertEquals(paymentRequest1.getFreelancerId(), requestDTO1.getFreelancerId()),
+        () -> assertEquals(paymentRequest2.getId(), requestDTO2.getId()),
+        () -> assertEquals(paymentRequest2.getAmount(), requestDTO2.getAmount()),
+        () -> assertEquals(paymentRequest2.getDescription(), requestDTO2.getDescription()),
+        () -> assertEquals(paymentRequest2.getClientId(), requestDTO2.getClientId()),
+        () -> assertEquals(paymentRequest2.getFreelancerId(), requestDTO2.getFreelancerId()));
+  }
+
+  @Test
+  void testGetFreelancerPaymentRequests() {
+    PaymentRequest paymentRequest1 =
+        PaymentRequest.builder()
+            .withAmount(1200)
+            .withDescription("Payment for services rendered")
+            .withClientId("3")
+            .withFreelancerId("4")
+            .build();
+    paymentRequestRepository.save(paymentRequest1);
+
+    PaymentRequest paymentRequest2 =
+        PaymentRequest.builder()
+            .withAmount(3600)
+            .withDescription("Payment for translation services")
+            .withClientId("10")
+            .withFreelancerId("4")
+            .build();
+    paymentRequestRepository.save(paymentRequest2);
+
+    GetFreelancerPaymentRequestsRequest getFreelancerPaymentRequests =
+        GetFreelancerPaymentRequestsRequest.builder().withFreelancerId("4").build();
+
+    GetFreelancerPaymentRequestsResponse response =
+        (GetFreelancerPaymentRequestsResponse)
+            template.convertSendAndReceive(
+                ServiceQueueNames.PAYMENTS, getFreelancerPaymentRequests);
+
+    assertNotNull(response);
+    assertEquals(HttpStatusCode.OK, response.getStatusCode());
+
+    assertNotNull(response.getRequests());
+    assertEquals(2, response.getRequests().size());
+
+    PaymentRequestDTO requestDTO1 = response.getRequests().get(0);
+    PaymentRequestDTO requestDTO2 = response.getRequests().get(1);
+
+    assertAll(
+        () -> assertEquals(paymentRequest1.getId(), requestDTO1.getId()),
+        () -> assertEquals(paymentRequest1.getAmount(), requestDTO1.getAmount()),
+        () -> assertEquals(paymentRequest1.getDescription(), requestDTO1.getDescription()),
+        () -> assertEquals(paymentRequest1.getClientId(), requestDTO1.getClientId()),
+        () -> assertEquals(paymentRequest1.getFreelancerId(), requestDTO1.getFreelancerId()),
+        () -> assertEquals(paymentRequest2.getId(), requestDTO2.getId()),
+        () -> assertEquals(paymentRequest2.getAmount(), requestDTO2.getAmount()),
+        () -> assertEquals(paymentRequest2.getDescription(), requestDTO2.getDescription()),
+        () -> assertEquals(paymentRequest2.getClientId(), requestDTO2.getClientId()),
+        () -> assertEquals(paymentRequest2.getFreelancerId(), requestDTO2.getFreelancerId()));
+  }
+
+  @Test
+  void testGetPaymentRequest() {
+    PaymentRequest paymentRequest =
+        PaymentRequest.builder()
+            .withAmount(1200)
+            .withDescription("Payment for services rendered")
+            .withClientId("3")
+            .withFreelancerId("4")
+            .build();
+    paymentRequestRepository.save(paymentRequest);
+
+    GetPaymentRequestRequest getPaymentRequest =
+        GetPaymentRequestRequest.builder().withPaymentRequestId(paymentRequest.getId()).build();
+
+    GetPaymentRequestResponse response =
+        (GetPaymentRequestResponse)
+            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, getPaymentRequest);
+
+    assertNotNull(response);
+    assertEquals(HttpStatusCode.OK, response.getStatusCode());
+
+    PaymentRequestDTO requestDTO = response.getRequest();
+
+    assertNotNull(requestDTO);
+
+    assertAll(
+        () -> assertEquals(paymentRequest.getId(), requestDTO.getId()),
+        () -> assertEquals(paymentRequest.getAmount(), requestDTO.getAmount()),
+        () -> assertEquals(paymentRequest.getDescription(), requestDTO.getDescription()),
+        () -> assertEquals(paymentRequest.getClientId(), requestDTO.getClientId()),
+        () -> assertEquals(paymentRequest.getFreelancerId(), requestDTO.getFreelancerId()));
+  }
+
+  @Test
+  void testGetPaymentRequestNotFound() {
+    GetPaymentRequestRequest getPaymentRequest =
+        GetPaymentRequestRequest.builder().withPaymentRequestId("123").build();
+
+    GetPaymentRequestResponse response =
+        (GetPaymentRequestResponse)
+            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, getPaymentRequest);
+
+    assertNotNull(response);
+    assertEquals(HttpStatusCode.NOT_FOUND, response.getStatusCode());
+    assertNull(response.getRequest());
+  }
+
+  @Test
+  void testPayPaymentRequest() {
+    PaymentRequest paymentRequest =
+        PaymentRequest.builder()
+            .withAmount(1200)
+            .withDescription("Payment for services rendered")
+            .withClientId("3")
+            .withFreelancerId("4")
+            .build();
+    paymentRequestRepository.save(paymentRequest);
+
+    Wallet wallet = Wallet.builder().withFreelancerId("4").build();
+    walletRepository.save(wallet);
+
+    PayPaymentRequestRequest payPaymentRequest =
+        PayPaymentRequestRequest.builder().withPaymentRequestId(paymentRequest.getId()).build();
+
+    PayPaymentRequestResponse response =
+        (PayPaymentRequestResponse)
+            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, payPaymentRequest);
+
+    assertNotNull(response);
+    assertEquals(HttpStatusCode.OK, response.getStatusCode());
+
+    assertNotNull(response.getTransactionId());
+    assertNotNull(response.getTransactionStatus());
+
+    assertEquals(PaymentTransactionStatus.SUCCESS, response.getTransactionStatus());
+
+    Optional<PaymentTransaction> optionalPaymentTransaction =
+        paymentTransactionRepository.findById(response.getTransactionId());
+
+    assertTrue(optionalPaymentTransaction.isPresent());
+
+    PaymentTransaction paymentTransaction = optionalPaymentTransaction.get();
+
+    assertAll(
+        () -> assertEquals(paymentRequest.getId(), paymentTransaction.getPaymentRequestId()),
+        () -> assertEquals(paymentRequest.getAmount(), paymentTransaction.getAmount()),
+        () -> assertEquals(PaymentTransactionStatus.SUCCESS, paymentTransaction.getStatus()));
+
+    Optional<PaymentRequest> optionalUpdatedPaymentRequest =
+        paymentRequestRepository.findById(paymentRequest.getId());
+
+    assertTrue(optionalUpdatedPaymentRequest.isPresent());
+
+    PaymentRequest updatedPaymentRequest = optionalUpdatedPaymentRequest.get();
+
+    assertEquals(PaymentRequestStatus.PAID, updatedPaymentRequest.getStatus());
+
+    Optional<Wallet> optionalUpdatedWallet = walletRepository.findById(wallet.getFreelancerId());
+
+    assertTrue(optionalUpdatedWallet.isPresent());
+
+    Wallet updatedWallet = optionalUpdatedWallet.get();
+
+    assertEquals(1200, updatedWallet.getBalance());
+
+    Optional<WalletTransaction> optionalWalletTransaction =
+        walletTransactionRepository.findAll().stream()
+            .filter(
+                transaction ->
+                    transaction.getWalletId().equals(wallet.getFreelancerId())
+                        && transaction.getPaymentTransactionId().equals(paymentTransaction.getId()))
+            .findFirst();
+
+    assertTrue(optionalWalletTransaction.isPresent());
+
+    WalletTransaction walletTransaction = optionalWalletTransaction.get();
+
+    assertAll(
+        () -> assertEquals(paymentRequest.getFreelancerId(), walletTransaction.getWalletId()),
+        () -> assertEquals(paymentRequest.getAmount(), walletTransaction.getAmount()),
+        () -> assertEquals(paymentTransaction.getId(), walletTransaction.getPaymentTransactionId()),
+        () -> assertEquals(paymentRequest.getDescription(), walletTransaction.getDescription()),
+        () -> assertEquals(WalletTransactionType.CREDIT, walletTransaction.getTransactionType()));
+  }
+
+  @Test
+  void testPayPaymentRequestNotFound() {
+    PayPaymentRequestRequest payPaymentRequest =
+        PayPaymentRequestRequest.builder().withPaymentRequestId("123").build();
+
+    PayPaymentRequestResponse response =
+        (PayPaymentRequestResponse)
+            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, payPaymentRequest);
+
+    assertNotNull(response);
+    assertEquals(HttpStatusCode.NOT_FOUND, response.getStatusCode());
+    assertNull(response.getTransactionId());
+    assertNull(response.getTransactionStatus());
+  }
+
+  @Test
+  void testPayPaymentRequestFreelancerWalletNotFound() {
+    PaymentRequest paymentRequest =
+        PaymentRequest.builder()
+            .withAmount(1200)
+            .withDescription("Payment for services rendered")
+            .withClientId("3")
+            .withFreelancerId("4")
+            .build();
+    paymentRequestRepository.save(paymentRequest);
+
+    PayPaymentRequestRequest payPaymentRequest =
+        PayPaymentRequestRequest.builder().withPaymentRequestId(paymentRequest.getId()).build();
+
+    PayPaymentRequestResponse response =
+        (PayPaymentRequestResponse)
+            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, payPaymentRequest);
+
+    assertNotNull(response);
+    assertEquals(HttpStatusCode.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    assertNull(response.getTransactionId());
+    assertNull(response.getTransactionStatus());
+  }
+
+  @Test
   void testCreateWalletCommand() {
 
-    CreateWalletRequest createWalletRequest = CreateWalletRequest.builder()
-        .withFreelancerId("1")
-        .build();
-    CreateWalletResponse response = (CreateWalletResponse) template.convertSendAndReceive(ServiceQueueNames.PAYMENTS,
-        createWalletRequest);
+    CreateWalletRequest createWalletRequest =
+        CreateWalletRequest.builder().withFreelancerId("1").build();
+    CreateWalletResponse response =
+        (CreateWalletResponse)
+            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, createWalletRequest);
     assertNotNull(response);
     assertEquals(HttpStatusCode.CREATED, response.getStatusCode());
-
   }
 
   @Test
   void testCreateDuplicateWalletIsInvalid() {
-    CreateWalletRequest createWalletRequest = CreateWalletRequest.builder()
-        .withFreelancerId("1")
-        .build();
-    CreateWalletResponse response = (CreateWalletResponse) template.convertSendAndReceive(ServiceQueueNames.PAYMENTS,
-        createWalletRequest);
+    CreateWalletRequest createWalletRequest =
+        CreateWalletRequest.builder().withFreelancerId("1").build();
+    CreateWalletResponse response =
+        (CreateWalletResponse)
+            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, createWalletRequest);
     assertNotNull(response);
     assertEquals(HttpStatusCode.CREATED, response.getStatusCode());
 
-    CreateWalletResponse response2 = (CreateWalletResponse) template.convertSendAndReceive(ServiceQueueNames.PAYMENTS,
-        createWalletRequest);
+    CreateWalletResponse response2 =
+        (CreateWalletResponse)
+            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, createWalletRequest);
     assertNotNull(response2);
     assertEquals(HttpStatusCode.BAD_REQUEST, response2.getStatusCode());
   }
 
   @Test
   void testGetValidWallet() {
-    CreateWalletRequest createWalletRequest = CreateWalletRequest.builder()
-        .withFreelancerId("1")
-        .build();
-    CreateWalletResponse response = (CreateWalletResponse) template.convertSendAndReceive(ServiceQueueNames.PAYMENTS,
-        createWalletRequest);
+    CreateWalletRequest createWalletRequest =
+        CreateWalletRequest.builder().withFreelancerId("1").build();
+    CreateWalletResponse response =
+        (CreateWalletResponse)
+            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, createWalletRequest);
     assertNotNull(response);
     assertEquals(HttpStatusCode.CREATED, response.getStatusCode());
 
-    GetWalletRequest getWalletRequest = GetWalletRequest.builder()
-        .withFreelancerId("1")
-        .build();
-    GetWalletResponse getWalletResponse = (GetWalletResponse) template.convertSendAndReceive(ServiceQueueNames.PAYMENTS,
-        getWalletRequest);
+    GetWalletRequest getWalletRequest = GetWalletRequest.builder().withFreelancerId("1").build();
+    GetWalletResponse getWalletResponse =
+        (GetWalletResponse)
+            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, getWalletRequest);
     assertNotNull(getWalletResponse);
     assertEquals(HttpStatusCode.OK, getWalletResponse.getStatusCode());
     assertEquals(0, getWalletResponse.getBalance());
@@ -155,13 +425,11 @@ class PaymentsApplicationTests {
 
   @Test
   void testGetInvalidWallet() {
-    GetWalletRequest getWalletRequest = GetWalletRequest.builder()
-        .withFreelancerId("1")
-        .build();
-    GetWalletResponse getWalletResponse = (GetWalletResponse) template.convertSendAndReceive(ServiceQueueNames.PAYMENTS,
-        getWalletRequest);
+    GetWalletRequest getWalletRequest = GetWalletRequest.builder().withFreelancerId("1").build();
+    GetWalletResponse getWalletResponse =
+        (GetWalletResponse)
+            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, getWalletRequest);
     assertNotNull(getWalletResponse);
     assertEquals(HttpStatusCode.NOT_FOUND, getWalletResponse.getStatusCode());
   }
-
 }
