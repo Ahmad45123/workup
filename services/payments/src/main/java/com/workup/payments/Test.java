7a828862f9b3f4c9a48929459473768c8e045a48
AhmedNasserG <ahmednasser217217@gmail.com>
2024-05-04 22:40:16 +0300

    configure file appenders

--- services/payments/src/main/java/com/workup/payments/Test.java
+++ services/payments/src/main/java/com/workup/payments/Test.java
@@ -7,9 +7,10 @@ public class Test {
   private static final Logger logger = LogManager.getLogger(Test.class);
 
   public static void main(String[] args) {
-    logger.info("Info level log message: Greeting endpoint was called");
+    logger.trace("Trace level log message: This shows more detailed information");
     logger.debug("Debug level log message: This shows more detailed information");
     logger.info("Info level log message: Greeting endpoint was called");
+    logger.warn("Info level log message: Greeting endpoint was called");
     logger.error("Error level log message: This shows more detailed information");
 
     System.out.println("Hello");
