aa7eb1853fcfaf48f25940d0b1dcebde9b78d4e9
Ahmed Elwasefi <a.m.elwasefi@gmail.com>
2024-05-06 01:12:29 +0300

    Added a lot

--- services/jobs/src/main/java/com/workup/jobs/JobsApplication.java
+++ services/jobs/src/main/java/com/workup/jobs/JobsApplication.java
@@ -4,6 +4,7 @@ import com.workup.shared.enums.ServiceQueueNames;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -23,5 +24,12 @@ public class JobsApplication {
   @Bean
   public MessageConverter messageConverter() {
     return new Jackson2JsonMessageConverter();
+  }
+
+  @Bean
+  public ApplicationRunner runner() {
+    return args -> {
+      System.out.println("WE ARE NEW IN JOBS");
+    };
   }
 }
