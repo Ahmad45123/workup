ef8ffdd1c4a7df31feb54ffe51f962b125c75a3c
Ziad Othman <ziadsadek999@gmail.com>
2024-05-05 22:01:30 +0300

    Controller shared interface (#69)

---
+++ shared/src/main/java/com/workup/shared/commands/controller/ContinueRequest.java
@@ -0,0 +1,6 @@
+package com.workup.shared.commands.controller;
+
+/** Makes a service start accepting requests and acquire resources again. */
+public class ContinueRequest {
+  // No fields are required?
+}
