94bb77f98be815debc669cbc69d5dfe38b28b5e2
AhmedNasserG <ahmednasser217217@gmail.com>
2024-05-05 18:56:19 +0300

    delete redis tests

--- services/payments/src/test/java/com/workup/payments/PaymentsApplicationTests.java
+++ services/payments/src/test/java/com/workup/payments/PaymentsApplicationTests.java
@@ -2,7 +2,6 @@ package com.workup.payments;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.redis.testcontainers.RedisContainer;
 import com.workup.payments.models.PaymentRequest;
 import com.workup.payments.models.PaymentTransaction;
 import com.workup.payments.models.Wallet;
@@ -12,8 +11,14 @@ import com.workup.payments.repositories.PaymentTransactionRepository;
 import com.workup.payments.repositories.WalletRepository;
 import com.workup.payments.repositories.WalletTransactionRepository;
 import com.workup.shared.commands.payments.dto.PaymentRequestDTO;
-import com.workup.shared.commands.payments.paymentrequest.requests.*;
-import com.workup.shared.commands.payments.paymentrequest.responses.*;
+import com.workup.shared.commands.payments.paymentrequest.requests.CreatePaymentRequestRequest;
+import com.workup.shared.commands.payments.paymentrequest.requests.GetClientPaymentRequestsRequest;
+import com.workup.shared.commands.payments.paymentrequest.requests.GetFreelancerPaymentRequestsRequest;
+import com.workup.shared.commands.payments.paymentrequest.requests.PayPaymentRequestRequest;
+import com.workup.shared.commands.payments.paymentrequest.responses.CreatePaymentRequestResponse;
+import com.workup.shared.commands.payments.paymentrequest.responses.GetClientPaymentRequestsResponse;
+import com.workup.shared.commands.payments.paymentrequest.responses.GetFreelancerPaymentRequestsResponse;
+import com.workup.shared.commands.payments.paymentrequest.responses.PayPaymentRequestResponse;
 import com.workup.shared.commands.payments.wallet.requests.CreateWalletRequest;
 import com.workup.shared.commands.payments.wallet.requests.GetWalletRequest;
 import com.workup.shared.commands.payments.wallet.responses.CreateWalletResponse;
@@ -31,7 +36,6 @@ import com.workup.shared.enums.ServiceQueueNames;
 import com.workup.shared.enums.payments.PaymentRequestStatus;
 import com.workup.shared.enums.payments.PaymentTransactionStatus;
 import com.workup.shared.enums.payments.WalletTransactionType;
-import com.workup.shared.redis.RedisService;
 import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.AfterAll;
@@ -47,7 +51,6 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @Testcontainers
@@ -62,16 +65,11 @@ class PaymentsApplicationTests {
   static final RabbitMQContainer rabbitMQContainer =
       new RabbitMQContainer("rabbitmq:3.13-management");
 
-  @Container
-  static final RedisContainer redisContainer =
-      new RedisContainer(DockerImageName.parse("redis:latest"));
-
   @Autowired private AmqpTemplate template;
   @Autowired private PaymentRequestRepository paymentRequestRepository;
   @Autowired private PaymentTransactionRepository paymentTransactionRepository;
   @Autowired private WalletRepository walletRepository;
   @Autowired private WalletTransactionRepository walletTransactionRepository;
-  @Autowired private RedisService redisService;
 
   @BeforeEach
   void clearAll() {
@@ -79,15 +77,12 @@ class PaymentsApplicationTests {
     paymentTransactionRepository.deleteAll();
     walletRepository.deleteAll();
     walletTransactionRepository.deleteAll();
-
-    redisService.clearCache();
   }
 
   @AfterAll
   static void stopContainers() {
     postgreSQLContainer.stop();
     rabbitMQContainer.stop();
-    redisContainer.stop();
   }
 
   @DynamicPropertySource
@@ -101,9 +96,6 @@ class PaymentsApplicationTests {
     registry.add("spring.rabbitmq.port", rabbitMQContainer::getFirstMappedPort);
     registry.add("spring.rabbitmq.username", rabbitMQContainer::getAdminUsername);
     registry.add("spring.rabbitmq.password", rabbitMQContainer::getAdminPassword);
-
-    registry.add("spring.redis.host", redisContainer::getHost);
-    registry.add("spring.redis.port", () -> redisContainer.getMappedPort(6379));
   }
 
   @Test
@@ -524,53 +516,6 @@ class PaymentsApplicationTests {
   }
 
   @Test
-  void testGetPaymentRequest() {
-    PaymentRequest paymentRequest =
-        PaymentRequest.builder()
-            .withAmount(1200)
-            .withDescription("Payment for services rendered")
-            .withClientId("3")
-            .withFreelancerId("4")
-            .build();
-    paymentRequestRepository.save(paymentRequest);
-
-    GetPaymentRequestRequest getPaymentRequest =
-        GetPaymentRequestRequest.builder().withPaymentRequestId(paymentRequest.getId()).build();
-
-    GetPaymentRequestResponse response =
-        (GetPaymentRequestResponse)
-            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, getPaymentRequest);
-
-    assertNotNull(response);
-    assertEquals(HttpStatusCode.OK, response.getStatusCode());
-
-    PaymentRequestDTO requestDTO = response.getRequest();
-
-    assertNotNull(requestDTO);
-
-    assertAll(
-        () -> assertEquals(paymentRequest.getId(), requestDTO.getId()),
-        () -> assertEquals(paymentRequest.getAmount(), requestDTO.getAmount()),
-        () -> assertEquals(paymentRequest.getDescription(), requestDTO.getDescription()),
-        () -> assertEquals(paymentRequest.getClientId(), requestDTO.getClientId()),
-        () -> assertEquals(paymentRequest.getFreelancerId(), requestDTO.getFreelancerId()));
-  }
-
-  @Test
-  void testGetPaymentRequestNotFound() {
-    GetPaymentRequestRequest getPaymentRequest =
-        GetPaymentRequestRequest.builder().withPaymentRequestId("123").build();
-
-    GetPaymentRequestResponse response =
-        (GetPaymentRequestResponse)
-            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, getPaymentRequest);
-
-    assertNotNull(response);
-    assertEquals(HttpStatusCode.NOT_FOUND, response.getStatusCode());
-    assertNull(response.getRequest());
-  }
-
-  @Test
   void testPayPaymentRequest() {
     PaymentRequest paymentRequest =
         PaymentRequest.builder()
@@ -743,52 +688,5 @@ class PaymentsApplicationTests {
             template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, getWalletRequest);
     assertNotNull(getWalletResponse);
     assertEquals(HttpStatusCode.NOT_FOUND, getWalletResponse.getStatusCode());
-  }
-
-  @Test
-  void testGetPaymentRequestResponseFromCache() {
-    PaymentRequest paymentRequest =
-        PaymentRequest.builder()
-            .withAmount(1200)
-            .withDescription("Payment for services rendered")
-            .withClientId("3")
-            .withFreelancerId("4")
-            .build();
-    paymentRequestRepository.save(paymentRequest);
-
-    GetPaymentRequestRequest getPaymentRequest =
-        GetPaymentRequestRequest.builder().withPaymentRequestId(paymentRequest.getId()).build();
-
-    GetPaymentRequestResponse response =
-        (GetPaymentRequestResponse)
-            template.convertSendAndReceive(ServiceQueueNames.PAYMENTS, getPaymentRequest);
-
-    assertNotNull(response);
-    assertEquals(HttpStatusCode.OK, response.getStatusCode());
-
-    GetPaymentRequestResponse cachedResponse =
-        (GetPaymentRequestResponse)
-            redisService.getValue(
-                getPaymentRequest.getPaymentRequestId(), GetPaymentRequestResponse.class);
-
-    assertNotNull(cachedResponse);
-    assertEquals(HttpStatusCode.OK, cachedResponse.getStatusCode());
-
-    assertAll(
-        () -> assertEquals(response.getRequest().getId(), cachedResponse.getRequest().getId()),
-        () ->
-            assertEquals(
-                response.getRequest().getAmount(), cachedResponse.getRequest().getAmount()),
-        () ->
-            assertEquals(
-                response.getRequest().getDescription(),
-                cachedResponse.getRequest().getDescription()),
-        () ->
-            assertEquals(
-                response.getRequest().getClientId(), cachedResponse.getRequest().getClientId()),
-        () ->
-            assertEquals(
-                response.getRequest().getFreelancerId(),
-                cachedResponse.getRequest().getFreelancerId()));
   }
 }
