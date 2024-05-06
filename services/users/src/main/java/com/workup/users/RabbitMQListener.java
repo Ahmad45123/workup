e63597b14b2549c2386d756ff9fbde4fd4a001e1
Mahmoud Gaber <mahmoudhosam44@gmail.com>
2024-05-06 21:00:18 +0300

    feat: Add function handlers to RabbitMQ

--- services/users/src/main/java/com/workup/users/RabbitMQListener.java
+++ services/users/src/main/java/com/workup/users/RabbitMQListener.java
@@ -5,6 +5,9 @@ import com.workup.shared.commands.users.responses.*;
 import com.workup.shared.enums.ServiceQueueNames;
 import com.workup.users.commands.*;
 import com.workup.users.commands.UserCommandMap;
+import com.workup.users.commands.responses.GetFreelancerAchievementsResponse;
+import com.workup.users.commands.responses.GetFreelancerEducationsResponse;
+import com.workup.users.commands.responses.GetFreelancerExperiencesResponse;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -61,5 +64,95 @@ public class RabbitMQListener {
   @RabbitHandler
   public ClientGetProfileResponse receive(ClientGetProfileRequest in) throws Exception {
     return ((ClientGetProfileCommand) commandMap.getCommand("ClientGetProfile")).Run(in);
+  }
+
+  @RabbitHandler
+  public AddFreelancerAchievementResponse receive(AddFreelancerAchievementRequest in) throws Exception {
+    return ((AddFreelancerAchievementCommand) commandMap.getCommand("AddFreelancerAchievement")).Run(in);
+  }
+
+  @RabbitHandler
+  public AddFreelancerEducationResponse receive(AddFreelancerEducationRequest in) throws Exception {
+    return ((AddFreelancerEducationCommand) commandMap.getCommand("AddFreelancerEducation")).Run(in);
+  }
+
+  @RabbitHandler
+  public AddFreelancerExperienceResponse receive(AddFreelancerExperienceRequest in) throws Exception {
+    return ((AddFreelancerExperienceCommand) commandMap.getCommand("AddFreelancerExperience")).Run(in);
+  }
+
+  @RabbitHandler
+  public AddFreelancerSkillResponse receive(AddFreelancerSkillRequest in) throws Exception {
+    return ((AddFreelancerSkillCommand) commandMap.getCommand("AddFreelancerSkill")).Run(in);
+  }
+
+  @RabbitHandler
+  public AddFreelancerLanguageResponse receive(AddFreelancerLanguageRequest in) throws Exception {
+    return ((AddFreelancerLanguageCommand) commandMap.getCommand("AddFreelancerLanguage")).Run(in);
+  }
+
+  @RabbitHandler
+  public GetFreelancerAchievementsResponse receive(GetFreelancerAchievementsRequest in) throws Exception {
+    return ((GetFreelancerAchievementsCommand) commandMap.getCommand("GetFreelancerAchievements")).Run(in);
+  }
+
+  @RabbitHandler
+  public GetFreelancerEducationsResponse receive(GetFreelancerEducationsRequest in) throws Exception {
+    return ((GetFreelancerEducationsCommand) commandMap.getCommand("GetFreelancerEducations")).Run(in);
+  }
+
+  @RabbitHandler
+  public GetFreelancerExperiencesResponse receive(GetFreelancerExperiencesRequest in) throws Exception {
+    return ((GetFreelancerExperiencesCommand) commandMap.getCommand("GetFreelancerExperiences")).Run(in);
+  }
+
+  @RabbitHandler
+  public GetFreelancerSkillsResponse receive(GetFreelancerSkillsRequest in) throws Exception {
+    return ((GetFreelancerSkillsCommand) commandMap.getCommand("GetFreelancerSkills")).Run(in);
+  }
+
+  @RabbitHandler
+  public GetFreelancerLanguagesResponse receive(GetFreelancerLanguagesRequest in) throws Exception {
+    return ((GetFreelancerLanguagesCommand) commandMap.getCommand("GetFreelancerLanguages")).Run(in);
+  }
+
+  @RabbitHandler
+  public UpdateFreelancerAchievementResponse receive(UpdateFreelancerAchievementRequest in) throws Exception {
+    return ((UpdateFreelancerAchievementCommand) commandMap.getCommand("UpdateFreelancerAchievement")).Run(in);
+  }
+
+  @RabbitHandler
+  public UpdateFreelancerEducationResponse receive(UpdateFreelancerEducationRequest in) throws Exception {
+    return ((UpdateFreelancerEducationCommand) commandMap.getCommand("UpdateFreelancerEducation")).Run(in);
+  }
+
+  @RabbitHandler
+  public UpdateFreelancerExperienceResponse receive(UpdateFreelancerExperienceRequest in) throws Exception {
+    return ((UpdateFreelancerExperienceCommand) commandMap.getCommand("UpdateFreelancerExperience")).Run(in);
+  }
+
+  @RabbitHandler
+  public RemoveFreelancerAchievementResponse receive(RemoveFreelancerAchievementRequest in) throws Exception {
+    return ((RemoveFreelancerAchievementCommand) commandMap.getCommand("RemoveFreelancerAchievement")).Run(in);
+  }
+
+  @RabbitHandler
+  public RemoveFreelancerEducationResponse receive(RemoveFreelancerEducationRequest in) throws Exception {
+    return ((RemoveFreelancerEducationCommand) commandMap.getCommand("RemoveFreelancerEducation")).Run(in);
+  }
+
+  @RabbitHandler
+  public RemoveFreelancerExperienceResponse receive(RemoveFreelancerExperienceRequest in) throws Exception {
+    return ((RemoveFreelancerExperienceCommand) commandMap.getCommand("RemoveFreelancerExperience")).Run(in);
+  }
+
+  @RabbitHandler
+  public RemoveFreelancerSkillResponse receive(RemoveFreelancerSkillRequest in) throws Exception {
+    return ((RemoveFreelancerSkillCommand) commandMap.getCommand("RemoveFreelancerSkill")).Run(in);
+  }
+
+  @RabbitHandler
+  public RemoveFreelancerLanguageResponse receive(RemoveFreelancerLanguageRequest in) throws Exception {
+    return ((RemoveFreelancerLanguageCommand) commandMap.getCommand("RemoveFreelancerLanguage")).Run(in);
   }
 }
