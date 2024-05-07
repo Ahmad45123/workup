2a606ba7b1e5e8e9177ea9279bd772b4e0cc39c3
shimaabetah <shimaabetah1911@gmail.com>
2024-05-08 02:16:53 +0300

    Added JWT Authentication

---
+++ webserver/src/main/java/com/workup/webserver/controller/jobs/UserController.java
@@ -0,0 +1,36 @@
+package com.workup.webserver.controller.jobs;
+
+import com.workup.webserver.config.JwtService;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.extern.jackson.Jacksonized;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+  @Autowired JwtService jwtService;
+
+  @PostMapping("/login")
+  public String postMethodName(@RequestBody UserLoginRequest request) {
+
+    // use jwy service to authenticate user assume usernameand password is right
+
+    String token = jwtService.generateToken(request.getUserName(), "123479797", "USER");
+
+    return token;
+  }
+}
+
+/** InnerUserController */
+@Builder
+@Jacksonized
+@Getter
+class UserLoginRequest {
+  String userName;
+  String password;
+}
