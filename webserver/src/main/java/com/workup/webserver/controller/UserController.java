dd870367d0f9217aaf2854c77987e13daa29403e
shimaabetah <shimaabetah1911@gmail.com>
2024-05-12 17:22:17 +0300

    Added Login and Register end points

--- webserver/src/main/java/com/workup/webserver/controller/UserController.java
+++ webserver/src/main/java/com/workup/webserver/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.workup.webserver.controller;
 
+import com.workup.shared.commands.users.requests.ClientRegisterRequest;
+import com.workup.shared.commands.users.requests.FreelancerRegisterRequest;
+import com.workup.shared.commands.users.requests.LoginRequest;
+import com.workup.shared.commands.users.responses.SignUpAndInResponse;
+import com.workup.shared.enums.ServiceQueueNames;
 import com.workup.webserver.config.JwtService;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.extern.jackson.Jacksonized;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +21,57 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
   @Autowired JwtService jwtService;
+  @Autowired AmqpTemplate rabbitTemplate;
 
   @PostMapping("/login")
-  public String postMethodName(@RequestBody UserLoginRequest request) {
+  public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
 
     // use jwy service to authenticate user assume usernameand password is right
+    SignUpAndInResponse response =
+        (SignUpAndInResponse)
+            rabbitTemplate.convertSendAndReceive(ServiceQueueNames.USERS, request);
+    if (response.isSuccess()) {
+      String token =
+          jwtService.generateToken(
+              response.getUserName(), response.getUserId(), response.getUserType().toString());
+      return ResponseEntity.ok(AuthResponse.builder().authToken(token).build());
+    }
+    return ResponseEntity.status(response.getStatusCode().getValue())
+        .body(AuthResponse.builder().errorMessage(response.getErrorMessage()).build());
+  }
 
-    String token = jwtService.generateToken(request.getUserName(), "123479797", "USER");
+  @PostMapping("/clients/register")
+  public ResponseEntity<AuthResponse> registerAsClient(@RequestBody ClientRegisterRequest request) {
 
-    return token;
+    // use jwy service to authenticate user assume usernameand password is right
+    SignUpAndInResponse response =
+        (SignUpAndInResponse)
+            rabbitTemplate.convertSendAndReceive(ServiceQueueNames.USERS, request);
+    if (response.isSuccess()) {
+      String token =
+          jwtService.generateToken(
+              response.getUserName(), response.getUserId(), response.getUserType().toString());
+      return ResponseEntity.ok(AuthResponse.builder().authToken(token).build());
+    }
+    return ResponseEntity.status(response.getStatusCode().getValue())
+        .body(AuthResponse.builder().errorMessage(response.getErrorMessage()).build());
+  }
+
+  @PostMapping("/freelancers/register")
+  public ResponseEntity<AuthResponse> postMethodName(
+      @RequestBody FreelancerRegisterRequest request) {
+    // use jwy service to authenticate user assume usernameand password is right
+    SignUpAndInResponse response =
+        (SignUpAndInResponse)
+            rabbitTemplate.convertSendAndReceive(ServiceQueueNames.USERS, request);
+    if (response.isSuccess()) {
+      String token =
+          jwtService.generateToken(
+              response.getUserName(), response.getUserId(), response.getUserType().toString());
+      return ResponseEntity.ok(AuthResponse.builder().authToken(token).build());
+    }
+    return ResponseEntity.status(response.getStatusCode().getValue())
+        .body(AuthResponse.builder().errorMessage(response.getErrorMessage()).build());
   }
 }
 
@@ -30,7 +79,7 @@ public class UserController {
 @Builder
 @Jacksonized
 @Getter
-class UserLoginRequest {
-  String userName;
-  String password;
+class AuthResponse {
+  String authToken;
+  String errorMessage;
 }
