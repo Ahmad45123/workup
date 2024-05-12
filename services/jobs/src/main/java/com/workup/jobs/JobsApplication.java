0204922e7574e28f6a90f481efd2aae2d41c25b5
Ahmed Elwasefi <a.m.elwasefi@gmail.com>
2024-05-12 23:45:46 +0300

    Added base controller.
    Main author: I just stole it :(
    Co-authored-by: Ziad Othman <ziadsadek999@gmail.com>

--- services/jobs/src/main/java/com/workup/jobs/JobsApplication.java
+++ services/jobs/src/main/java/com/workup/jobs/JobsApplication.java
@@ -1,15 +1,18 @@
 package com.workup.jobs;
 
+import com.workup.shared.enums.ControllerQueueNames;
 import com.workup.shared.enums.ServiceQueueNames;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
+@EnableAsync
 public class JobsApplication {
 
   public static void main(String[] args) {
@@ -22,14 +25,33 @@ public class JobsApplication {
   }
 
   @Bean
+  public Queue controllerQueue() {
+    return new AnonymousQueue();
+  }
+
+  @Bean
+  public FanoutExchange fanout() {
+    return new FanoutExchange(ControllerQueueNames.JOBS);
+  }
+
+  @Bean
+  public Binding fanoutBinding(FanoutExchange fanout, Queue controllerQueue) {
+    return BindingBuilder.bind(controllerQueue).to(fanout);
+  }
+
+  @Bean
   public MessageConverter messageConverter() {
     return new Jackson2JsonMessageConverter();
   }
 
   @Bean
-  public ApplicationRunner runner() {
-    return args -> {
-      System.out.println("WE ARE NEW IN JOBS");
-    };
+  public ThreadPoolTaskExecutor taskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(50);
+    executor.setMaxPoolSize(50);
+    executor.setQueueCapacity(500);
+    executor.setThreadNamePrefix("jobs-");
+    executor.initialize();
+    return executor;
   }
 }
